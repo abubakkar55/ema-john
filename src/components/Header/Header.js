3585c92c424c294d354cf113a6ab27f89ca07df9
Ab <computerm246@gmail.com>
2021-09-25 12:13:30 +0600

    header completed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,21 +4,21 @@ const  logo =  "images/logo.png";
 const Header = () => {
     return (
         <div>
-<div className="container">
-    <div> <img src={logo} alt="" /> </div>
-    <div> 
-    <input type="text" placeholder="Search your desire product"
-    // onChange={props.searchText}
-
-     /> </div>
+<div className="header-container flex items-center justify-between px-20 h-20 bg-white shadow-md">
+    <div> <img src={logo} className="w-48" alt="" /> </div>
+    <div className="w-2/6"> 
+    <input className="bg-white-50 shadow-md w-full h-12 ring-2  ring-yellow-300 p-4 rounded outline-none" type="text" placeholder="Search your desire product"
+    // onChange={props.searchText} 
+    
+    /> </div>
     <div>
         <nav>
-            <ul>
-                <li><a href="/home"> Home</a></li>
-                <li><a href="/about">About </a></li>
-                <li><a href="/products">Products </a></li>
-                <li><a href="/contact">Contact us </a></li>
-                <li><a href="/"> <FaShoppingCart /> </a></li>
+            <ul className="flex items-center">
+                <li><a className="mr-10 font-semibold text-xl" href="/home"> Home</a></li>
+                <li><a className="mr-10 font-semibold text-xl" href="/about">About </a></li>
+                <li><a className="mr-10 font-semibold text-xl" href="/products">Products </a></li>
+                <li><a className="mr-10 font-semibold text-xl" href="/contact">Contact us </a></li>
+                <li><a className="text-xl flex items-center" href="/"> <FaShoppingCart /> <span className="ml-2 text-yellow-400">0 </span>   </a></li>
             </ul>
         </nav>
     </div>
